f44a13572a88551e647c3860fbea4c5b647f4250
JamesKawashima <89648605+JamesKawashima@users.noreply.github.com>
2022-11-03 14:53:13 -0400

    Added mouse and constructor

---
+++ AppDevFinal/AppDevFinal/Mouse.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppDevFinal
+{
+    internal class Mouse
+    {
+        private static int idNum = 0;
+        private int id;
+        private int height;
+        private int width;
+        private int weight;
+        private int experimentId;
+        private string experimentName;
+        private string doseName;
+        private int doseML;
+
+        public Mouse(int height, int width, int weight, int experimentId, 
+            string experimentName, string doseName, int doseML)
+        {
+
+            this.id = id++;
+            this.height = height;
+            this.width = width;
+            this.weight = weight;
+            this.experimentId = experimentId;
+            this.experimentName = experimentName;
+            this.doseName = doseName;
+            this.doseML = doseML;
+        }
+
+        public Mouse(int height, int width, int weight)
+        {
+            this.id = id++;
+            this.height = height;
+            this.height = height;
+            this.width = width;
+            this.weight = weight;
+        }
+    }
+}
