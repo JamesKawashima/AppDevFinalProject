609221b21fe7f5cb9502551addd9785ec5f456f8
Amanda De Rosa <goldyblockslol@gmail.com>
2022-12-10 13:58:22 -0500

    recreating project

---
+++ TSO App Dev Project/TSO App Dev Project/StudiesForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TSO_App_Dev_Project
+{
+    public partial class StudiesForm : Form
+    {
+        public StudiesForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
